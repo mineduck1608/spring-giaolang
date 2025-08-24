b097464376031b9e968b262c8acce762040737f8
mineduck1608 <ducdm160804@gmail.com>
2025-08-24 16:00:32 +0700

    1:39:55

---
+++ Session01-Maven/maven-to-go/src/main/java/com/mineduck1608/basis/maventogo/MavenToGoApplication.java
@@ -0,0 +1,13 @@
+package com.mineduck1608.basis.maventogo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenToGoApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+}
---
+++ Session01-Maven/maven-to-go/src/test/java/com/mineduck1608/basis/maventogo/MavenToGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mineduck1608.basis.maventogo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenToGoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
